9d54905af71bd4d6f19291584336f5f45d8a3040
Kenil Gondaliya <kenilgondaliya960@gmail.com>
2025-09-14 22:45:17 +0530

    commit

---
+++ package/package1/A.java
@@ -0,0 +1,18 @@
+package Package1;
+
+public class A {   // <-- must be public to use in another package
+   private int x;
+   private int y;
+
+   public A() { }   // constructor must also be public
+
+   public void setData(int var1, int var2) {
+      this.x = var1;
+      this.y = var2;
+   }
+
+   public void display() {
+      System.out.println("x: " + this.x);
+      System.out.println("y: " + this.y);
+   }
+}
---
+++ package/package2/B.java
@@ -0,0 +1,12 @@
+package Package2;
+
+import Package1.A;
+
+public class B {
+   public static void main(String[] args) {
+      System.out.println("Hello B");
+      A a1 = new A();
+      a1.setData(5, 6);
+      a1.display();
+   }
+}
